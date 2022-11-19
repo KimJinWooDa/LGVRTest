68202216812504b9377978eac455571128aaa63d
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-11-19 17:01:36 +0900

    yyyyhy

---
+++ Assets/Scripts/UserInfoManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfoManager : Singleton<UserInfoManager>
+{
+    public string userName;
+    //public Image 
+}
