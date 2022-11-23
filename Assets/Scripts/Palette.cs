d8a247f1617dd565b0722bda91f6626b8d3d7c32
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-11-23 23:44:58 +0900

    [Feat] 버그 수정 및 개선
    키보드 Hover Material 삭제
    키보드 BoxProximity 수치 1 -> 0.01로 수치 변경
    비행기 등장 파티클 개수 조정 및 위치 조정
    팔레트 위치 급격하게 변하면 이상한 곳으로 날라가는 버그 수정
    하이라이트 _ 1손가락 -> 5손가락 가능

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -1,11 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Palette : Singleton<Palette>
 {
+    public Transform hand;
+    public Transform origin;
     public Material[] brushColor;
     public GameObject[] inkColor;
     
+    private Vector3 OriginPosition;
+    
+    void Start()
+    {
+        OriginPosition = this.transform.localPosition;
+    }
 
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(this.transform.position, hand.transform.position) > 1f)
+        {
+            this.transform.position = origin.position;
+        }
+    }
 }
